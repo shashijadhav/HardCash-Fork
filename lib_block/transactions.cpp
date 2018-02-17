ce4e974307814c08afc50de03380237ab84225d0
CodeObsess <SpawnTree@users.noreply.github.com>
2018-02-17 19:54:41 +0530

    Update transactions.cpp

--- lib_block/transactions.cpp
+++ lib_block/transactions.cpp
@@ -3,3 +3,43 @@
 //
 
 #include "transactions.h"
+template <class T1, class T2>
+struct pair {
+    // ...
+    constexpr pair() noexcept(
+        is_nothrow_constructible<T1>::value &&
+        is_nothrow_constructible<T2>::value );
+ 
+    pair(const pair&) = default;
+ 
+    pair(const T1& x, const T2& y) noexcept(  
+        is_nothrow_constructible<T1, const T1&>::value &&
+        is_nothrow_constructible<T2, const T2&>::value );
+    //...
+    void swap(pair& p) noexcept( 
+        noexcept(swap(first, p.first)) &&
+        noexcept(swap(second, p.second)));
+};
+
+void fun1() noexcept {
+    int i = 0;    // noexcept
+    int * p = &i; // noexcept
+    p = nullptr;  // noexcept
+}
+ 
+void fun2() noexcept {
+    fun1();
+    fun1();
+}
+ 
+int main() {
+    std::unique_ptr p( new int(1) );
+    fun2();
+    // (*) 
+}
+
+template <typename T, typename Params...>  
+T awesome_factory(Params... params) noexcept(noexcept(T{params...}))  
+{
+   return T{params...};
+}
