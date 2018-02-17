9da215fbaaee1771536851883dd3a2856322cc01
CodeObsess <SpawnTree@users.noreply.github.com>
2018-02-17 19:55:44 +0530

    Update transactions.cpp

--- lib_block/transactions.cpp
+++ lib_block/transactions.cpp
@@ -2,44 +2,81 @@
 // Created by SUMIT LAHIRI on 5/1/17.
 //
 
-#include "transactions.h"
-template <class T1, class T2>
-struct pair {
-    // ...
-    constexpr pair() noexcept(
-        is_nothrow_constructible<T1>::value &&
-        is_nothrow_constructible<T2>::value );
+// C++ program to demonstrate lambda expression in C++
+#include <bits/stdc++.h>
+using namespace std;
  
-    pair(const pair&) = default;
- 
-    pair(const T1& x, const T2& y) noexcept(  
-        is_nothrow_constructible<T1, const T1&>::value &&
-        is_nothrow_constructible<T2, const T2&>::value );
-    //...
-    void swap(pair& p) noexcept( 
-        noexcept(swap(first, p.first)) &&
-        noexcept(swap(second, p.second)));
-};
-
-void fun1() noexcept {
-    int i = 0;    // noexcept
-    int * p = &i; // noexcept
-    p = nullptr;  // noexcept
-}
- 
-void fun2() noexcept {
-    fun1();
-    fun1();
-}
- 
-int main() {
-    std::unique_ptr p( new int(1) );
-    fun2();
-    // (*) 
-}
-
-template <typename T, typename Params...>  
-T awesome_factory(Params... params) noexcept(noexcept(T{params...}))  
+// Function to print vector
+void printVector(vector<int> v)
 {
-   return T{params...};
+    // lambda expression to print vector
+    for_each(v.begin(), v.end(), [](int i)
+    {
+        std::cout << i << " ";
+    });
+    cout << endl;
+}
+ 
+int main()
+{
+    vector<int> v {4, 1, 3, 5, 2, 3, 1, 7};
+ 
+    printVector(v);
+ 
+    // below snippet find first number greater than 4
+    // find_if searches for an element for which
+    // function(third argument) returns true
+    vector<int>:: iterator p = find_if(v.begin(), v.end(), [](int i)
+    {
+        return i > 4;
+    });
+    cout << "First number greater than 4 is : " << *p << endl;
+ 
+ 
+    // function to sort vector, lambda expression is for sorting in
+    // non-decreasing order Compiler can make out return type as
+    // bool, but shown here just for explanation
+    sort(v.begin(), v.end(), [](const int& a, const int& b) -> bool
+    {
+        return a > b;
+    });
+ 
+    printVector(v);
+ 
+    // function to count numbers greater than or equal to 5
+    int count_5 = count_if(v.begin(), v.end(), [](int a)
+    {
+        return (a >= 5);
+    });
+    cout << "The number of elements greater than or equal to 5 is : "
+         << count_5 << endl;
+ 
+    // function for removing duplicate element (after sorting all
+    // duplicate comes together)
+    p = unique(v.begin(), v.end(), [](int a, int b)
+    {
+        return a == b;
+    });
+ 
+    // resizing vector to make size equal to total different number
+    v.resize(distance(v.begin(), p));
+    printVector(v);
+ 
+    // accumulate function accumulate the container on the basis of
+    // function provided as third argument
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int f = accumulate(arr, arr + 10, 1, [](int i, int j)
+    {
+        return i * j;
+    });
+ 
+    cout << "Factorial of 10 is : " << f << endl;
+ 
+    //   We can also access function by storing this into variable
+    auto square = [](int i)
+    {
+        return i * i;
+    };
+ 
+    cout << "Square of 5 is : " << square(5) << endl;
 }
